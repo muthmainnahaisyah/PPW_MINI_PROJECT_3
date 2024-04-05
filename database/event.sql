033c1d9a36abfa8b55ed8fafc947d87b3a6162aa
muthmainnahaisyah <muthmainnah01@gmail.com>
2024-04-05 12:45:47 +0800

    update database

--- database/event.sql
+++ database/event.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2024 at 11:54 AM
+-- Generation Time: Apr 05, 2024 at 06:33 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `event`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun`
+--
+
+CREATE TABLE `akun` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`id`, `name`, `email`, `password`, `user_type`) VALUES
+(1, 'muthmainnah', 'muth01@gmail.com', '1111', 'admin'),
+(2, 'aisyah', 'aisyah01@gmail.com', '0000', 'user'),
+(3, 'kayla', 'kayla@gmail.com', '1234', 'user');
 
 -- --------------------------------------------------------
 
@@ -48,9 +71,56 @@ CREATE TABLE `event` (
 INSERT INTO `event` (`id`, `id_user`, `nama`, `no_hp`, `jenis_acara`, `tanggal_mulai`, `tanggal_selesai`, `total_biaya`, `deskripsi_acara`, `gambar`, `konfirmasi`) VALUES
 (15, 2, 'Aisyah', '081234567890', 'pernikahan', '2024-04-10', '2024-04-10', 300000, '<p>Mengajukan acara dengan tema wedding seperti pada gambar</p>', '../img/acara/event_wedding.jpg', '');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL,
+  `kategori` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `kategori`) VALUES
+(2, 'wisuda'),
+(5, 'training'),
+(7, 'pernikahan'),
+(8, 'syukuran'),
+(9, 'pembukaan brand');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tempat_event`
+--
+
+CREATE TABLE `tempat_event` (
+  `id` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tempat_event`
+--
+
+INSERT INTO `tempat_event` (`id`, `id_admin`, `harga`) VALUES
+(1, 1, 300000);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `event`
@@ -59,14 +129,44 @@ ALTER TABLE `event`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tempat_event`
+--
+ALTER TABLE `tempat_event`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tempat_event`
+--
+ALTER TABLE `tempat_event`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
